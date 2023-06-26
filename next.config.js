bb105a9ac80e454343e75da3383cb05d65326819
Dat Lieu <dat.lieu@nfq.asia>
2023-06-26 10:45:26 +0700

    MAKAIRA-3563 update run symfony and nextjs can run parallel in heroku

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-// require('dotenv').config({ path: '.env' })
+require('dotenv').config({ path: '.env' })
 
 const nextConfig = {
   reactStrictMode: true,
